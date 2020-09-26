fb9b6d9fbde33e2161dfb2908136fff3a3c94a98
pokabu55 <okazumaru@gmail.com>
2020-09-26 20:01:40 +0900

    型の修正

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,19 @@
 
 int main()
 { 
-    time_t t = time(NULL);
-    printf("%s", ctime(&t));
-    printf("raw = %ld\n", t);
+    time_t t1 = time(NULL);
+    printf("%s", ctime(&t1));
+    printf("raw = %ld\n", t1);
+
+
+	int32_t t2 = 1601113393;
+	int32_t t3 = 1701113393;
+
+    int32_t sub2 = t1 - t2;
+    printf("sub = %d\n", sub2);
+    int32_t sub3 = t1 - t3;
+    printf("sub = %d\n", sub3);
+
 
     return 0;
 }
